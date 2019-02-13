6f7e2eb135078d949c5bd24c95ddfac0647d8d45
Arnau Sanchez <pyarnau@gmail.com>
2019-02-13 10:19:20 +0100

    Add jest

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-function add(x: number, y: number): number {
+export function add(x: number, y: number): number {
     return x + y;
-}
-
-export function answer(): number {
-    return add(40, 2);
 }
