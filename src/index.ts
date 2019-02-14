4ce0ca9c8e5306e3b8775cd89dd95f9dfe7bde0f
Arnau Sanchez <pyarnau@gmail.com>
2019-02-14 12:12:07 +0100

    Add DialogHandler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { DatePicker } from "./date-picker/DatePicker";
 import MultiSelector from "./multi-selector/MultiSelector";
 import SimpleCheckBox from "./simple-check-box/SimpleCheckBox";
+import DialogHandler from "./dialog-handler/DialogHandler";
 
-export { DatePicker, MultiSelector, SimpleCheckBox };
+export { DatePicker, MultiSelector, SimpleCheckBox, DialogHandler };
