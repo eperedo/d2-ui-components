ff7f334b06201d5f5e5b5b57ed83f71b95427b07
Alexis Rico <sferadev@gmail.com>
2019-02-21 18:35:30 +0100

    DataTable: Add lodash deepMerge mixin

---
+++ src/utils/lodash.js
@@ -0,0 +1,15 @@
+import _ from "lodash";
+
+function deepMerge(object, source) {
+    return _.mergeWith(object, source, function(objValue, srcValue) {
+        if (_.isObject(objValue) && srcValue) {
+            return deepMerge(objValue, srcValue);
+        }
+    });
+}
+
+_.mixin({
+    deepMerge,
+});
+
+export default _;
