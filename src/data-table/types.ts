66acd71e6e46a3172e6059e75d98fe801674fa8e
Arnau Sanchez <pyarnau@gmail.com>
2019-11-25 09:58:26 +0100

    DRY TableInitialState interface

--- src/data-table/types.ts
+++ src/data-table/types.ts
@@ -36,19 +36,17 @@ export interface TablePagination {
     page: number;
 }
 
-type Optional<T, K> = { [P in Extract<keyof T, K>]?: T[P] };
-
 export interface TableState<T extends ReferenceObject> {
     selection: string[];
     sorting: TableSorting<T>;
     pagination: TablePagination;
 }
 
-export interface TableInitialState<T extends ReferenceObject> {
-    selection?: string[];
-    sorting?: TableSorting<T>;
-    pagination?: Optional<TablePagination, "pageSizeOptions" | "pageSize" | "total" | "page">;
-}
+export type TableInitialState<T extends ReferenceObject> = Partial<
+    Omit<TableState<T>, "pagination">
+> & {
+    pagination?: Partial<TablePagination>;
+};
 
 export interface TableNotification {
     // These props should be refactored and included everything into (...args) => ReactNode
