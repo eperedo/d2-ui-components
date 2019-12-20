9e9328dcd6a894fc33aeba3d618c10f41098eed9
Arnau Sanchez <pyarnau@gmail.com>
2019-12-20 10:43:01 +0100

    Update jest key and format

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	roots: [ '<rootDir>/src' ],
-	transform: {
-		"^.+\\.(js|jsx)$": "babel-jest",
-		'^.+\\.tsx?$': 'ts-jest'
-	},
-	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-	moduleFileExtensions: [ '	ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
-	transformIgnorePatterns: [],
-	snapshotSerializers: ["enzyme-to-json/serializer"],
-	setupTestFrameworkScriptFile: "<rootDir>/setupEnzyme.js",
+    roots: ["<rootDir>/src"],
+    transform: {
+        "^.+\\.(js|jsx)$": "babel-jest",
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    moduleFileExtensions: ["	ts", "tsx", "js", "jsx", "json", "node"],
+    transformIgnorePatterns: [],
+    snapshotSerializers: ["enzyme-to-json/serializer"],
+    setupFilesAfterEnv: ["<rootDir>/setupEnzyme.js"],
 };
