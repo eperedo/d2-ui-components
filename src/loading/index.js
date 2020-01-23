7a0231d9ad63e34d49883e2e5aa4438bf3dfd1a2
Alexis Rico <sferadev@gmail.com>
2020-01-23 11:18:16 +0100

    Refactor Snackbar and Loading providers into TS

--- src/loading/index.js
+++ src/loading/index.js
@@ -1,13 +1,18 @@
-import React from "react";
-import context from "./context";
+import React, { useContext } from "react";
+import loadingContext from "./context";
 
 export function withLoading(WrappedComponent) {
     return class extends React.Component {
         static displayName = `withLoading${WrappedComponent.displayName}`;
-        static contextType = context;
+        static contextType = loadingContext;
 
         render() {
             return <WrappedComponent {...this.props} loading={this.context} />;
         }
     };
+}
+
+export function useLoading() {
+    const contextValue = useContext(loadingContext);
+    return contextValue;
 }
---
+++ src/snackbar/SnackbarProvider.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import SnackbarContext from "./context";
+import SnackbarConsumer from "./SnackbarConsumer";
+
+type SnackbarLevel = "success" | "info" | "warning" | "error";
+
+interface SnackbarOptions {
+    isOpen: boolean;
+    message?: string;
+    variant?: SnackbarLevel;
+    autoHideDuration?: number;
+}
+
+export const SnackbarProvider = ({ children }) => {
+    const [state, setState] = useState<SnackbarOptions>({
+        isOpen: false,
+        message: "",
+        variant: "success",
+    });
+
+    const openSnackbar = (
+        variant: SnackbarLevel,
+        message: string,
+        options: Partial<SnackbarOptions> = {}
+    ) => {
+        const defaultAutoHideDuration = variant === "success" ? 2000 : undefined;
+        const autoHideDuration = options.hasOwnProperty("autoHideDuration")
+            ? options.autoHideDuration
+            : defaultAutoHideDuration;
+
+        setState({
+            isOpen: true,
+            message,
+            variant,
+            autoHideDuration,
+        });
+    };
+
+    const byLevel = {
+        success: (message: string, options: Partial<SnackbarOptions> = {}) => {
+            openSnackbar("success", message, options);
+        },
+        info: (message: string, options: Partial<SnackbarOptions> = {}) => {
+            openSnackbar("info", message, options);
+        },
+        warning: (message: string, options: Partial<SnackbarOptions> = {}) => {
+            openSnackbar("warning", message, options);
+        },
+        error: (message: string, options: Partial<SnackbarOptions> = {}) => {
+            openSnackbar("error", message, options);
+        },
+    };
+
+    const closeSnackbar = () => {
+        setState(state => ({
+            ...state,
+            message: "",
+            isOpen: false,
+        }));
+    };
+
+    const value = {
+        openSnackbar: openSnackbar,
+        byLevel: byLevel,
+        closeSnackbar: closeSnackbar,
+        snackbarIsOpen: state.isOpen,
+        message: state.message,
+        variant: state.variant,
+        autoHideDuration: state.autoHideDuration,
+    };
+
+    return (
+        <SnackbarContext.Provider value={value}>
+            <SnackbarConsumer />
+            {children}
+        </SnackbarContext.Provider>
+    );
+};
+
+export default SnackbarProvider;
