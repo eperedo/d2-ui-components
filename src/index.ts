579045d2c399e3ea3ec6a63c534dff8cdbbd83ed
p3rcypj <pablo@percypj.com>
2022-04-19 09:33:26 +0200

    Update dependencies and i18n

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
-import DialogButton from "./dialog-button/DialogButton";
-import MultiSelector from "./multi-selector/MultiSelector";
-import OrgUnitsSelector from "./org-units-selector/OrgUnitsSelector";
-import SimpleCheckBox from "./simple-check-box/SimpleCheckBox";
 import "./locales";
 
-export * from "./confirmation-dialog/ConfirmationDialog";
-export * from "./data-table/DataTable";
-export * from "./data-table/ObjectsTable";
-export * from "./data-table/types";
-export * from "./date-picker/DatePicker";
-export * from "./dropdown";
-export * from "./loading";
-export * from "./search-box/SearchBox";
-export * from "./snackbar";
-export * from "./wizard/Wizard";
-export * from "./sharing/Sharing";
-export * from "./objects-list/ObjectsList";
-export * from "./objects-list/objects-list-hooks";
-
-export { MultiSelector, SimpleCheckBox, DialogButton, OrgUnitsSelector };
+export { SimpleCheckBox } from "./simple-check-box/SimpleCheckBox";
